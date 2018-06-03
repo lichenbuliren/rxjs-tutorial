071c0264044b12fa18d0dd6f33392e459d22a616
lichenbuliren <617106268@qq.com>
2018-06-03 19:12:05 +0800

    feat(all): 初始化 rxjs 项目

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 ## Rxjs 探索
+
+### 环境安装
+
+全局安装编译脚本：
+``` bash
+yarn global add parcel-bundler
+```
+
+启动编译：
+``` bash
+npm start
+```
--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('hello world')
+import { Observable, of } from 'rxjs'
+import { map } from 'rxjs/operators/'
+// import { of } from 'rxjs/observable/of'
+
+const source$ = of(1,2,3)
+source$.subscribe(console.log, null, () => console.log('completed'))
+
+source$.pipe(map((value, i) => `${value}_${i}`)).subscribe(console.log)
